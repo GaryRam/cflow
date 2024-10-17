9c2f9f05a42095c727399e8e09547c84966642cb
GaryRam <126gramirez@frhsd.com>
2024-10-17 14:03:32 -0400

    lag

--- js/sandbox.js
+++ js/sandbox.js
@@ -51,9 +51,73 @@
 //     console.log("that's a lot of ninjas");
 // }
 
-const password = 'p@ss';
+// const password = 'password';
 
-if(password.length >= 8){
-    console.log('that password is long enough!');
+// if(password.length >= 12 && password.includes('@')){
+//     console.log('that password is mighty strong');
+// } else if(password.lenght >= 8 || password.includes('@') && password.length >= 5){
+//     console.log('that password is strong enough!');
+// } else{
+//     console.log('password is not strong enough');
+// }
+
+// logical NOT (!)
+
+// let user = false;
+
+// if(!user){
+//     console.log('you must be logged in to continue');
+// }
+
+// console.log(!true);
+// console.log(!false);
+
+
+// break and continue
+
+// const scores = [50, 25, 0, 30, 100, 20, 10];
+
+// for(let i = 0; i < scores.length; i++){
+
+//     if(scores[i] === 0){
+//         continue;
+//     }
+
+//     console.log('your score: ', scores[i]);
+
+//     if(scores[i] === 100){
+//         console.log('congrats, you got the top score!');
+//         break;
+//     }
+    
+// }
+
+// switch statements
+// const grade = '50';
+
+// switch(grade){
+//     case 50:
+//         console.log('you got an A!');
+//         break;
+//     case 'B':
+//         console.log('you got an B!');
+//         break;
+//     case 'C':
+//         console.log('you got an C!');
+//         break;
+//     case 'D':
+//         console.log('you got an D!');
+//         break;
+//     case 'E':
+//         console.log('you got an E!');
+//         break;
+//     default:
+//         console.log('not a valid grade');
+// }
+
+//variables & block scope
+const age = 30;
+
+if(true){
     
 }
